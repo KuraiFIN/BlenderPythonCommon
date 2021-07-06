4055487508f238adccc8fbd650294b7fe550db68
KuraiFIN <73153015+KuraiFIN@users.noreply.github.com>
2021-07-06 14:20:49 -0600

    Update terrain_tiles.py

--- terrain_tiles.py
+++ terrain_tiles.py
@@ -1,3 +1,6 @@
+# This file does many things: creating vertex weights, randomizing weights, bleeding weights of neighboring tiles into each other, calculating projected uvs of objects en masse, etc
+
+
 import bpy
 import bmesh
 import random
